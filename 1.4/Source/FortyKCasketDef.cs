5dde9d99ceba90c1fb61b2a91961f2de3348b239
papercrane1001 <papercrane1001@gmail.com>
2023-08-16 14:10:32 -0700

    Fixed bug where it wasn't reading the xml def as the right type... because I didn't tell it to.

--- 1.4/Source/FortyKCasketDef.cs
+++ 1.4/Source/FortyKCasketDef.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using VFEPirates;
+
 using Verse;
 
 namespace Grimforge
 {
-    public class FortyKCasketDef : ThingDef
+    public class FortyKCasketDef : WarcasketDef
     {
+        //Based on NanosuitDef
         public float maxEnergyAmount;
         //public List<Ability_Active> ac
         public List<Ability_Passive> passive_Abilities = new List<Ability_Passive>();
--- 1.4/Source/Gizmo_ArmorEnergyStatus.cs
+++ 1.4/Source/Gizmo_ArmorEnergyStatus.cs
@@ -33,7 +33,7 @@ namespace Grimforge
         }
 
 
-        //Getting an "Object reference not set to an instance of an object" on this function.  This disables 
+        //Getting an "Object reference not set to an instance of an object" on this function.  
         public override GizmoResult GizmoOnGUI(Vector2 topLeft, float maxWidth, GizmoRenderParms parms)
         {
             //throw new NotImplementedException();
@@ -46,14 +46,23 @@ namespace Grimforge
 
             Text.Font = GameFont.Tiny;
             //TODO: 1 Label text.  In Nanosuit, this is "nanosuit.LabelShortCap"
-            Widgets.Label(rect3, "TestLabelTODO1");
+            //Widgets.Label(rect3, "TestLabelTODO1");
+            Widgets.Label(rect3, casket.Energy.ToString());
 
             var rect4 = rect2;
             rect4.yMin = rect2.y + (rect2.height / 2f);
 
+            //Debug.Log(casket.Energy.ToString());
+            //Verse.Log:Warning
+            Log.Message("Test2");
+            Log.Message(casket.def.maxEnergyAmount.ToString());
+
             //TODO: Implement
-            //var fillPercent = casket.Energy / casket.def.maxEnergyAmount;
-            var fillPercent = 0.5f;
+            var fillPercent = casket.Energy / casket.def.maxEnergyAmount;
+            //var fillPercent = 0.5f;
+
+            Log.Message("Test3");
+
             Widgets.FillableBar(rect4, fillPercent, FullEnergyBarTex, EmptyEnergyBarTex, false);
             Text.Font = GameFont.Small;
             Text.Anchor = TextAnchor.MiddleCenter;
