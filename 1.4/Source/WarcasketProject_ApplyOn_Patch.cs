827102083c9b21867125c83b26c76dbe316938b4
papercrane1001 <papercrane1001@gmail.com>
2023-08-16 15:40:14 -0700

    Yeah, still broken.  Pretty sure the answer is in WarcasketProject_ApplyOn_Patch

--- 1.4/Source/WarcasketProject_ApplyOn_Patch.cs
+++ 1.4/Source/WarcasketProject_ApplyOn_Patch.cs
@@ -10,9 +10,9 @@ namespace Grimforge
         public static void Postfix(WarcasketProject __instance, Pawn pawn)
         {
             var data = __instance.GetAdditionalWarcasketProjectData();
-            var shoulder = pawn.apparel.WornApparel.FirstOrDefault(x => x.def == __instance.shoulderPadsDef) as Apparel_WarcasketGrimforge;
-            var armor = pawn.apparel.WornApparel.FirstOrDefault(x => x.def == __instance.armorDef) as Apparel_WarcasketGrimforge;
-            var helmet = pawn.apparel.WornApparel.FirstOrDefault(x => x.def == __instance.helmetDef) as Apparel_WarcasketGrimforge;
+            var shoulder = pawn.apparel.WornApparel.FirstOrDefault(x => x.def == __instance.shoulderPadsDef) as Apparel_WarcasketGrimforge_Pads;
+            var armor = pawn.apparel.WornApparel.FirstOrDefault(x => x.def == __instance.armorDef) as Apparel_WarcasketGrimforge_Body;
+            var helmet = pawn.apparel.WornApparel.FirstOrDefault(x => x.def == __instance.helmetDef) as Apparel_WarcasketGrimforge_Helm;
             if (shoulder != null)
             {
                 shoulder.colorApparelTwo = data.shoulderColorTwo;
