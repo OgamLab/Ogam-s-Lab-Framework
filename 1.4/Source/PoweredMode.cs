80b9683a213cb8ba69bfd834656f7c2463da0de3
papercrane1001 <papercrane1001@gmail.com>
2023-08-26 14:29:43 -0700

    minor stuff

--- 1.4/Source/PoweredMode.cs
+++ 1.4/Source/PoweredMode.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
+
+using RimWorld;
 
 namespace Grimforge
 {
@@ -11,10 +14,19 @@ namespace Grimforge
     /// For specialised caskets like Techmarine add bonus to production quality, work speed, research speed
     /// And all of this shoud be as simple and low ticks as possible
     /// </summary>
-    public class PoweredMode
+    public class PoweredMode : Ability_Passive
     {
+        //Consider combining?
         public float carryCapacityBonus; // = 100f;
         public float moveSpeedBonus;// = 0.5f;
         public float selfHealFactor;
+
+        public PoweredMode()
+        {
+            Name = "PoweredMode".Translate();
+            Label = "PoweredMode".Translate();
+            Description = "PoweredModeDesc".Translate();
+            //Drain
+        }
     }
 }
