8f86501b21d5d35f6ccdbb06a1bf0aff03e95075
papercrane1001 <papercrane1001@gmail.com>
2023-08-28 13:15:51 -0700

    Mostly notes.  Need to investigate stat offsets.

--- 1.4/Source/Ability_Passive.cs
+++ 1.4/Source/Ability_Passive.cs
@@ -12,12 +12,31 @@ using Verse;
 
 namespace Grimforge
 {
-    //public enum Ability_Enum
-    //{
-    //    Test
-    //}
+    /// <summary>
+    /// "Void shield"   Simple shield with massive HP pool but with slow regeneration (entire shield need 1/2 to 1 hours to recharge)
+    /// "Invisibility"
+    /// "Healing specialist"     Aura which speedup tending wounds even tending wounds itself
+    ///+50% speed/quality tend
+    ///+50% success surgery rate
+    ///"Psyker" 
+    ///He can cast spells from mamy magic schools
+    ///100% Meditation psyfocus gain 
+    ///+ 50 % Neural heat recovery rate
+    ///+ 25 % Psychic Sensitivity
+    ///"Techmarine"
+    ///+ 5 to craft
+    ///+ 50 % to craft/smith/trailor speed
+    ///1+ to craft quality
+    ///Chaplain
+    ///+5 to social
+    ///+100% suppression
+    ///+75% to social impact
+
+
+    /// </summary>
     public abstract class Ability_Passive
     {
+        public Pawn pawn;
         public string Name { get; set; }
         public bool Active { get; set; }
         public float Drain { get; set; }
@@ -37,15 +56,23 @@ namespace Grimforge
             Active = state;
         }
 
+        public Ability_Passive(Pawn wearer)
+        {
+            pawn = wearer;
+        }
+
     }
 
     public class TestPassive : Ability_Passive
     {
-        public TestPassive()
+        public TestPassive(Pawn wearer) : base(wearer)
         {
             Name = "TestPassiveName";
             Active = false;
             Drain = 0.1f;
+            
+            //pawn.GetStatValue()
+            //StatDef.
         }
     }
 }
