523b7e9f21948c251c6eb853966edf5dd5f5e94a
papercrane1001 <papercrane1001@gmail.com>
2023-09-21 13:01:51 -0700

    Working on JobDefOf

---
+++ 1.4/Source/GF_JobDefOf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+
+namespace Grimforge
+{
+    public class GF_JobDefOf
+    {
+        static GF_JobDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(GF_JobDefOf));
+        }
+
+        public static JobDef GF_GetRecharge;
+
+
+
+    }
+}
